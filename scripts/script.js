80de9e4ae4502fd7f7734d5f1c8dd023b3b96852
JorgeMagno <jorge.magnolm@gmail.com>
2022-05-07 11:56:11 -0300

    adição da função de potencia

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 let display = document.querySelector('#display');
 
 function showDisplay(value) {
-    if (value == '.') {
+    if (value == '.' && display.value == "") {
         display.value = '0'
     }
     display.value += value;
